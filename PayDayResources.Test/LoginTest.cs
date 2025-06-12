1631d059c7b0ea91d70eed045a77a1f025774396
jsieiropd <jsieiropd@users.noreply.github.com>
2025-06-12 09:46:01 -0500

    Added new class with login test with simplified sintax. But it still doesnt have the browser, screenshot and record video functionalities.

---
+++ PayDayResources.Test/LoginTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace PayDayResources.Test
+{
+    public class LoginTest : PageTest
+    {
+        [SetUp]
+        public async Task Setup()
+        {
+            await Page.GotoAsync("http://10.10.24.122:8888/Login");
+        }
+
+        [Test]
+        public async Task Login()
+        {
+            //Rellenar credenciales
+            await Page.FillAsync("#LoginInput_Email", "jsieiro@payday.com.pa");
+            await Page.FillAsync("#LoginInput_Password", "PayDay2021*");
+
+            //Click en Iniciar Sesión (sin esperar nada más)
+            await Page.ClickAsync("text=Iniciar Sesión");
+
+            //Verificar la existencia de una opcion de la barra del menú que solo es visible cuando se está loggueado
+            await Expect(Page.Locator("#configDropdown")).ToBeVisibleAsync();
+        }
+    }
+}
